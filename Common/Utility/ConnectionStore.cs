4446324dfe15d1b4b37b3da835c2679a7c5e28d7
INSOLUTIONS\MichalG <mgwozdz@insolutions.pl>
2017-10-31 22:10:01 +0100

    ConnectionStore fix

--- Common/Utility/ConnectionStore.cs
+++ Common/Utility/ConnectionStore.cs
@@ -6,8 +6,19 @@ namespace Common.Utility
     {
         public static string MachineName =>
             Environment.MachineName;
-        public static string ConnectionString =>
-            $"Data Source=LukaszS-PC\\SQL2016;Initial Catalog=Szmidt1013;Integrated Security=SSPI;persist security info=false;MultipleActiveResultSets=True;Connect Timeout=10";
+        public static string ConnectionString
+        {
+            get
+            {
+                switch (MachineName)
+                {
+                    case "LukaszS-PC":
+                        return $"Data Source=LukaszS-PC\\SQL2016;Initial Catalog=Szmidt1013;Integrated Security=SSPI;persist security info=false;MultipleActiveResultSets=True;Connect Timeout=10";
+                    default:
+                        return $"Data Source={MachineName};Initial Catalog=Dapper;Integrated Security=SSPI;persist security info=false;MultipleActiveResultSets=True;Connect Timeout=10";
+                }
+            }
+        }
         public static string ConnectionErrorProvider(string message) =>
             $"Wystąpił problem z połączeniem \n {message}";
     }
