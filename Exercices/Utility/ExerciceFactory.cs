fab853a24266d03ba2607dbdb3800a5693981725
INSOLUTIONS\MichalG <mgwozdz@insolutions.pl>
2017-11-12 22:16:52 +0100

    changes in exercice factory

--- Exercices/Utility/ExerciceFactory.cs
+++ Exercices/Utility/ExerciceFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Common.Enum;
 using Exercices.Exercices.Exercice_1;
+using Exercices.Exercices.Exercise_2;
+using Exercices.Exercices.Exercise_3;
 using Exercices.Exercices.Interface;
 
 namespace Exercices.Utility
@@ -18,6 +20,12 @@ namespace Exercices.Utility
             {
                 case EExercice.Exercice_1:
                     choice = new Exercice_1();
+                    break;
+                case EExercice.Exercice_2:
+                    choice = new Exercice_2();
+                    break;
+                case EExercice.Exercice_3:
+                    choice = new Exercice_3();
                     break;
                 case EExercice.Quit:
                 default: break;
