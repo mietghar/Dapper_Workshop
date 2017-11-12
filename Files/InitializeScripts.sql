e3bc351981844f98eb9f354f5ae5dd492ce8a07c
Łukasz Szustak <lszustak@insolutions.pl>
2017-11-12 21:23:20 +0100

    Dodacznie trzeciego ćwiczenia

--- Files/InitializeScripts.sql
+++ Files/InitializeScripts.sql
@@ -52,6 +52,7 @@ CREATE TABLE [dbo].[Employee](
 
 	INSERT [dbo].[Employee] ([UserId], [AddressId], [FirstName], [LastName]) VALUES (1, 1, 'Jan', 'Kowalski')
 	INSERT [dbo].[Employee] ([UserId], [AddressId], [FirstName], [LastName]) VALUES (3, 2, 'Mietek', 'Gwozdz')
+	INSERT [dbo].[Employee] ([UserId], [AddressId], [FirstName], [LastName]) VALUES (2, 1, 'Łukasz', 'Szustak')
 END
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[User]') AND type in (N'U'))
@@ -70,4 +71,72 @@ CREATE TABLE [dbo].[User](
 	INSERT [dbo].[User] ([UserName], [Password], [ModifiedDate]) VALUES ('Janex', 'secretPassword', '2017-10-30')
 	INSERT [dbo].[User] ([UserName], [Password], [ModifiedDate]) VALUES ('UserSecond', 'anotherPassword', '2017-09-30')
 	INSERT [dbo].[User] ([UserName], [Password], [ModifiedDate]) VALUES ('Mietghar', 'abcd', '2017-10-15')
+END
+
+IF NOT EXISTS (SELECT * FROM Employee)
+BEGIN
+INSERT INTO Employee (UserId, AddressId, FirstName, LastName) 
+VALUES (1, 1, 'Jan', 'Kowalski'), (3, 2, 'Mietek', 'Gwozdz'), (2, 1, 'Łukasz', 'Szustak')
+END
+
+IF NOT EXISTS (SELECT * FROM Address)
+BEGIN
+INSERT INTO Address (City, PostCode, Street, HouseNumber, LocalNumber) 
+VALUES ('Katowice', '41-xxx', 'Katowicka', '1', '2'), ('Zabrze', '41-xxx', 'Zabrzanska', '11', '22')
+END
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[EmployeeInsert]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[EmployeeInsert]
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddressSave]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[AddressSave]
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddressUpdate]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[AddressUpdate]
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddressUpdate]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[AddressUpdate]
+@AddressId int,
+@City varchar(100),
+@PostCode varchar(10),
+@Street varchar(100),
+@HouseNumber int,
+@LocalNumber int = null
+AS
+BEGIN
+	UPDATE Address
+	SET City = @City, PostCode = @PostCode, Street = @Street, HouseNumber = @HouseNumber, LocalNumber = @LocalNumber
+	WHERE AddressId = @AddressId
+END'
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddressSave]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[AddressSave]
+@City varchar(100),
+@PostCode varchar(10),
+@Street varchar(100),
+@HouseNumber int,
+@LocalNumber int = null
+AS
+BEGIN
+	INSERT INTO Address (City, PostCode, Street, HouseNumber, LocalNumber)
+	Values (@City, @PostCode, @Street, @HouseNumber, @LocalNumber)
+END'
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[EmployeeInsert]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[EmployeeInsert]
+@FirstName varchar(50),
+@LastName varchar(50) = null,
+@AddressId int,
+@UserId int
+AS
+BEGIN
+IF NOT EXISTS (SELECT EmployeeID FROM Employee WHERE FirstName = @FirstName AND LastName = @LastName)
+BEGIN INSERT INTO Employee (UserId, FirstName, LastName, AddressId)
+Values (@UserId, @FirstName, @LastName, @AddressId) END
+END'
 END
