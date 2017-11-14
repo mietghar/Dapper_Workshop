9a4a2ca1fc41429a5758ea23366dc7af7ac78806
INSOLUTIONS\MichalG <mgwozdz@insolutions.pl>
2017-11-14 21:37:37 +0100

    added exercice 5

---
+++ Exercices/Exercices/Exercice_5/Exercice_5.cs
@@ -0,0 +1,19 @@
+﻿using Exercices.Exercices.Interface;
+using System;
+
+namespace Exercices.Exercices.Exercice_5
+{
+    public class Exercice_5 : IExerciceChoice
+    {
+        /// <summary>
+        /// Method should execute multimapping 1 to 1 query All of Employee Table and User Table where
+        /// Employee.UserId is equal to User.Id
+        /// </summary>
+        /// <returns></returns>
+        public object RunExercice()
+        {
+
+            return 0;
+        }
+    }
+}
